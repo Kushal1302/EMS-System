e4c49b4a5f131a3ea1091f2016c1664f9b4090cf
Kushal Kumar <kushalmalviya1302@gmail.com>
2024-04-11 19:48:28 +0530

    Add leave router and home page message

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = express()
 import adminRouter from './routes/adminRouter.js'
 import employeeRouter from './routes/employeeRouter.js'
 import loginRouter from './routes/loginRouter.js'
+import leaveRouter from './routes/leaveRouter.js'
 import cors from 'cors'
 app.use(cors())
 app.use(express.json())
@@ -14,5 +15,10 @@ const PORT = process.env.PORT || 3000
 app.use('/admin' , adminRouter)
 app.use('/employee' , employeeRouter)
 app.use('/login' , loginRouter)
+app.use('/leave' , leaveRouter )
+
+app.get('/' , (req ,res) => {
+    return res.send("EMS Server is live")
+})
 
 app.listen(PORT , () => console.log("Listening at port No.",PORT))
